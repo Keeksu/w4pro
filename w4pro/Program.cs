f17d01034b7b6da870d21ccb227ebcc181eaea88
Keeksu <gregory.kask@gmail.com>
2024-12-15 00:02:06 +0200

    kei

---
+++ w4pro/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int i = 0;
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("We don't need no education");
+}
+
+Console.WriteLine(i);
+Console.WriteLine("have a nice day");
+
+//Console.WriteLine("We don't need no education");
+//Console.WriteLine("We don't need no education");
+//Console.WriteLine("We don't need no education");
+//Console.WriteLine("We don't need no education");
+//Console.WriteLine("We don't need no education");
